be3214ce627603233e58838deb9494dce3c63c59
MRossa <maxim.rossa@mail.ru>
2024-01-28 15:01:51 +0400

    Rewritten methods for adding and retrieving keys from Redis

--- app/pkg/redds_tools/tools.py
+++ app/pkg/redds_tools/tools.py
@@ -1,16 +1,23 @@
 import redis
+from typing import Tuple
 
 class RedisTools:
     
     __redis_connect = redis.Redis(host='redis', port=6379)
     
     @classmethod
-    def set_data(cls, title: str, text: str):
-        cls.__redis_connect.set(title, text)
+    def set_file_data(cls, title: str, data: Tuple[str, str, float]):
+        '''
+        Метод для добавления данных о файле в Rabit
+        Пример
+        title: str = 'Хорошая книга'
+        data = [timestamp, book_title, x_avg_count_in_line]
+        '''
+        cls.__redis_connect.rpush(title, *data)
         
     @classmethod
-    def get_data(cls, title):
-        return cls.__redis_connect.get(title)
+    def get_data(cls, key):
+        return cls.__redis_connect.lrange(key, 0, -1)
     
     @classmethod
     def get_keys(cls):
